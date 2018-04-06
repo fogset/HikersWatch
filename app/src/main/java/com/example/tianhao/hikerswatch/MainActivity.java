f84543a9f56a19bafcf62028dd6a065f0f48680e
Tianhao <zhangth1990@hotmail.com>
2018-04-05 23:52:52 -0600

    Added accuracy and altitude

--- app/src/main/java/com/example/tianhao/hikerswatch/MainActivity.java
+++ app/src/main/java/com/example/tianhao/hikerswatch/MainActivity.java
@@ -17,31 +17,37 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     LocationManager locationManager;
     LocationListener locationListener;
-    TextView latitude, longtitude;
+    TextView latitude, longtitude,accuracy, altitude, address;
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-            }
+            startListening();
         }
     }
-
+    public void startListening(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         latitude = findViewById(R.id.latitude);
         longtitude = findViewById(R.id.longtitude);
+        accuracy = findViewById(R.id.acc);
+        altitude = findViewById(R.id.alt);
+        address = findViewById(R.id.add);
+
+
+
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                Log.i("Location", location.toString());
-                latitude.setText("Latitude: " +String.valueOf(location.getLatitude()));
-                longtitude.setText("Longtitude: " +String.valueOf(location.getLongitude()));
+                updatLocationInfo(location);
             }
 
             @Override
@@ -63,7 +69,18 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
         }else{
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastKnownLocation != null){
+                updatLocationInfo(lastKnownLocation);
+            }
         }
 
+    }
+    public void updatLocationInfo(Location location){
+        Log.i("Last know location", location.toString());
+        latitude.setText("Latitude: " +String.valueOf(location.getLatitude()));
+        longtitude.setText("Longtitude: " +String.valueOf(location.getLongitude()));
+        accuracy.setText("Accuracy: " +String.valueOf(location.getAccuracy()));
+        altitude.setText("Altitude: " +String.valueOf(location.getAltitude()));
     }
 }
